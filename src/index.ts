2c3f1e1dcb821943275f4e9ca7d99eea0935e960
Craig Dennis <craigsdennis@gmail.com>
2024-09-17 10:31:06 -0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
 
-export default {
-	async fetch(_request, _env, _ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{Bindings: Env}>();
+
+app.get("/api", (c) => {
+	console.log("Hi Mom!");
+	return c.text("Hello, Hono!");
+})
+export default app;
