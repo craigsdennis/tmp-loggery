e828d753b4cc231e9728c5914d95793c9846c9d6
Craig Dennis <craigsdennis@gmail.com>
2024-09-17 12:56:27 -0400

    More hiiiii

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono<{Bindings: Env}>();
 
 app.get("/api", (c) => {
-	console.log("Hi Mom!");
+	console.log("Hi Mom!!!!");
 	return c.text("Hello, Hono!");
 })
 export default app;
